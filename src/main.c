33b1c5fcf850d979c340530fd1e7d666efb740ef
PrajwalCH <prajjwal2058@gmail.com>
2021-11-17 18:31:59 +0545

    refactor: add new function to test request parser

--- src/main.c
+++ src/main.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include "request.h"
 
-int main(int argc, char **argv)
+static void test_request_parser(void)
 {
-    char *req_msg = "GET /id/1200?name=prajwal HTTP/1.1\r\nContent-Length: 12\r\n";
-    Request req = request_parse(argv[1] ? argv[1] : req_msg);
+    char raw_request[] = "GET /test/api HTTP/1.1\r\nHost: http://localhost:8000\r\nAccept: text/html\r\nConnection: Alive\r\n";
 
-    printf("method: %s\nuri: %s\nprotocol: %s\n", req.method, req.uri, req.protocol);
+    printf("---TEST REQUEST PARSER---\n");
+    printf("Input: \n");
+    printf("%s\n", raw_request);
+    Request req = request_parse(raw_request);
     
-    printf("\n---Headers---\n");
-    printf("headers size: %u\n", req.headers_size);
+    printf("Output: \n");
+    printf("Method -> %s\n", req.method);
+    printf("Uri -> %s\n", req.uri);
+    printf("Protocol -> %s\n", req.protocol);
+
+    printf("\n");
+
+    printf("Headers size: %u\n", req.headers_size);
     for (size_t i = 0; i < req.headers_size; i++) {
         printf("%s -> %s\n", req.headers[i]->name, req.headers[i]->value);
     }
+    request_dealloc(&req);
+}
 
-    request_dealloc_parsed(&req);
+int main(int argc, char **argv)
+{
+    test_request_parser();
     return 0;
 }
 
